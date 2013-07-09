7443f2fffcab4c6fce4b3c06e864a9a0d6ed2fd5
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-10 11:20:10 +1200

    Histogram figure

--- Figures/normal.py
+++ Figures/normal.py
@@ -1,9 +1,10 @@
 import numpy as np
-import scipy.misc
+import numpy.random as rng
 import matplotlib.pyplot as plt
 
 plt.rc("font", size=16, family="serif", serif="Computer Sans")
 plt.rc("text", usetex=True)
+rng.seed(123)
 
 theta = np.linspace(-10., 10., 101)
 post = np.exp(-0.5*(theta - 1.)**2)
@@ -13,5 +14,13 @@ plt.plot(theta, post, 'bo-', markersize=5)
 plt.xlabel('Theta')
 plt.ylabel('Posterior Probability')
 plt.savefig('normal.pdf', bbox_inches='tight')
+plt.show()
+
+theta = 1. + rng.randn(1000)
+print(np.mean(theta), np.std(theta))
+plt.hist(theta, 100)
+plt.xlabel('Theta')
+plt.ylabel('Number')
+plt.savefig('normal2.pdf', bbox_inches='tight')
 plt.show()
 
