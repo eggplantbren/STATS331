fb3afb1fe96f0e9c9e6021a08d4621fadd5c355c
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-12 14:36:25 +1200

    Make figures for road data

--- Code/Regression/main.r
+++ Code/Regression/main.r
@@ -4,6 +4,8 @@ library('rjags')
 # Load my make_list function
 source('../make_list.r')
 
+set.seed(1234)
+
 # Load data from file
 data = source('data.txt')$value
 
@@ -14,8 +16,15 @@ m = jags.model(file='model.txt', data=data)
 update(m, 10000)
 
 # Do the MCMC for real
-draw = jags.samples(m, 100000, thin=10, variable.names = c('beta0', 'beta1', 'sigma'))
+draw = jags.samples(m, 5000, thin=10, variable.names = c('beta0', 'beta1', 'sigma'))
 
 # Convert to a list
 results = make_list(draw)
+
+# Save as a text file
+chains = array(NA, dim=c(500, 3))
+chains[,1] = draw$beta0
+chains[,2] = draw$beta1
+chains[,3] = draw$sigma
+write.table(chains, 'chains.txt', row.names=FALSE, col.names=FALSE)
 
