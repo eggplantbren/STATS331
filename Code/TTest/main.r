a40e9e709b5be97bc56dd369e6663a0db50161b0
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-13 19:13:54 +1200

    TTest results figure

--- Code/TTest/main.r
+++ Code/TTest/main.r
@@ -10,10 +10,10 @@ source('data.txt')
 # Run model 1 and save results
 m = jags.model(file='model1.txt', data=data)
 update(m, 10000)
-draw = jags.samples(m, 100000, thin=10,
+draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
 results = make_list(draw)
-chains = array(NA, dim=c(10000, 3))
+chains = array(NA, dim=c(1000, 3))
 chains[,1] = results$mu1
 chains[,2] = results$mu2
 chains[,3] = results$sigma
@@ -22,10 +22,10 @@ write.table(chains, file='chains1.txt', row.names=FALSE, col.names=FALSE)
 # Run model 2 and save results
 m = jags.model(file='model2.txt', data=data)
 update(m, 10000)
-draw = jags.samples(m, 100000, thin=10,
+draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
 results = make_list(draw)
-chains = array(NA, dim=c(10000, 3))
+chains = array(NA, dim=c(1000, 3))
 chains[,1] = results$mu1
 chains[,2] = results$mu2
 chains[,3] = results$sigma
@@ -34,10 +34,10 @@ write.table(chains, file='chains2.txt', row.names=FALSE, col.names=FALSE)
 # Run model 3 and save results
 m = jags.model(file='model3.txt', data=data)
 update(m, 10000)
-draw = jags.samples(m, 100000, thin=10,
+draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
 results = make_list(draw)
-chains = array(NA, dim=c(10000, 3))
+chains = array(NA, dim=c(1000, 3))
 chains[,1] = results$mu1
 chains[,2] = results$mu2
 chains[,3] = results$sigma
