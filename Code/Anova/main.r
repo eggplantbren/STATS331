070faf4eb9307a57b74f4ceec43552199c3282e7
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-14 16:06:01 +1200

    Starling data

--- Code/Anova/main.r
+++ Code/Anova/main.r
@@ -8,7 +8,9 @@ source('../make_list.r')
 
 # Load fake data from file
 data = source('starling_data.txt')$value
-
+boxplot(data$y[data$group==1], data$y[data$group==2], data$y[data$group==3], data$y[data$group==4],
+				xlab='Location', ylab='Mass (grams)', main='Starling Data',
+				names=c(1, 2, 3, 4))
 # Create a model
 m = jags.model(file='model.txt', data=data)
 
@@ -25,4 +27,11 @@ results = make_list(draw)
 
 # Plot (samples from) the posterior distribution for sigma
 hist(results$sigma, breaks=100, xlab='Sigma', ylab='Number')
+
+chains = array(NA, dim=c(10000, 7))
+chains[,1:4] = results$mu
+chains[,5] = results$sigma
+chains[,6] = results$grand_mean
+chains[,7] = results$log_diversity
+write.table(chains, 'chains.txt', row.names=FALSE, col.names=FALSE)
 
