56929dc2f996fdd5a73ddf4ca13f521f44f82295
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-14 13:11:29 +1200

    Prior samples as well

--- Code/TTest/main.r
+++ Code/TTest/main.r
@@ -1,11 +1,50 @@
 # Load the rjags library
 library('rjags')
 
+set.seed(123)
+
 # Load my make_list function
 source('../make_list.r')
 
 # Load data from file
 source('data.txt')
+
+# Run model 1 and save results
+m = jags.model(file='model1.txt', data=list(N1=data$N1, N2=data$N2))
+update(m, 10000)
+draw = jags.samples(m, 100000, thin=100,
+		variable.names = c('mu1', 'mu2', 'sigma'))
+results = make_list(draw)
+chains = array(NA, dim=c(1000, 3))
+chains[,1] = results$mu1
+chains[,2] = results$mu2
+chains[,3] = results$sigma
+write.table(chains, file='prior1.txt', row.names=FALSE, col.names=FALSE)
+
+# Run model 2 and save results
+m = jags.model(file='model2.txt', data=list(N1=data$N1, N2=data$N2))
+update(m, 10000)
+draw = jags.samples(m, 100000, thin=100,
+		variable.names = c('mu1', 'mu2', 'sigma'))
+results = make_list(draw)
+chains = array(NA, dim=c(1000, 3))
+chains[,1] = results$mu1
+chains[,2] = results$mu2
+chains[,3] = results$sigma
+write.table(chains, file='prior2.txt', row.names=FALSE, col.names=FALSE)
+
+# Run model 3 and save results
+m = jags.model(file='model3.txt', data=list(N1=data$N1, N2=data$N2))
+update(m, 10000)
+draw = jags.samples(m, 100000, thin=100,
+		variable.names = c('mu1', 'mu2', 'sigma'))
+results = make_list(draw)
+chains = array(NA, dim=c(1000, 3))
+chains[,1] = results$mu1
+chains[,2] = results$mu2
+chains[,3] = results$sigma
+write.table(chains, file='prior3.txt', row.names=FALSE, col.names=FALSE)
+
 
 # Run model 1 and save results
 m = jags.model(file='model1.txt', data=data)
