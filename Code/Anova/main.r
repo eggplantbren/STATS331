8242799343de995a217e63ac07f98b6031f959d2
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-07-14 14:04:53 +1200

    Starling data for one-way ANOVA

--- Code/Anova/main.r
+++ Code/Anova/main.r
@@ -5,7 +5,7 @@ library('rjags')
 source('../make_list.r')
 
 # Load fake data from file
-data = source('data.txt')$value
+data = source('starling_data.txt')$value
 
 # Create a model
 m = jags.model(file='model.txt', data=data)
@@ -15,7 +15,7 @@ update(m, 10000)
 
 # Do the MCMC for real
 draw = jags.samples(m, 100000, thin=10,
-		variable.names = c('mu1', 'mu2', 'mu3', 'sigma',
+		variable.names = c('mu', 'sigma',
 			'grand_mean', 'log_diversity'))
 
 # Convert to a list
@@ -23,5 +23,4 @@ results = make_list(draw)
 
 # Plot (samples from) the posterior distribution for sigma
 hist(results$sigma, breaks=100, xlab='Sigma', ylab='Number')
-
 
