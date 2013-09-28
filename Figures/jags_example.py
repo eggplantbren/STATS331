878df2a7069288106281e6658da8cbe9dea0275f
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-09-28 16:09:26 +1200

    New figure of basic MCMC

---
+++ Figures/jags_example.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt
+
+plt.rc("font", size=16, family="serif", serif="Computer Sans")
+plt.rc("text", usetex=True)
+
+theta = np.loadtxt('jags_example.txt')
+
+plt.figure(figsize=(8, 7))
+plt.subplot(2,1,1)
+plt.plot(theta[0:500], linewidth=1)
+plt.xlabel('Iteration')
+plt.ylabel('theta')
+
+plt.subplot(2,1,2)
+plt.hist(theta, 100, alpha=0.5)
+plt.xlabel('theta')
+plt.ylabel('Frequency')
+
+plt.savefig('jags_example.pdf', bbox_inches='tight')
+plt.show()
+
