462b4040faf447edff84aab8ff495d6003114351
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-11-26 15:12:24 +1300

    Newest version of use_jags.r

---
+++ Code/use_jags.r
@@ -0,0 +1,60 @@
+model = "model
+{
+  theta ~ dunif(0, 1)
+  x ~ dbin(theta, N)
+}
+"
+
+# The data (use NA for no data)
+data = list(x=2, N=5)
+
+# Variables to monitor
+variable_names = c('theta')
+
+# How many burn-in steps?
+burn_in = 1000
+
+# How many proper steps?
+steps = 10000
+
+# Thinning?
+thin = 1
+
+
+
+
+# NO NEED TO EDIT PAST HERE!!!
+# Just run it all and use the results list.
+
+library('rjags')
+
+# Write model out to file
+fileConn=file("model.temp")
+writeLines(model, fileConn)
+close(fileConn)
+
+if(all(is.na(data)))
+{
+	m = jags.model(file="model.temp")
+} else
+{
+	m = jags.model(file="model.temp", data=data)
+}
+update(m, burn_in)
+draw = jags.samples(m, steps, thin=thin, variable.names = variable_names)
+# Convert to a list
+make_list <- function(draw)
+{
+	results = list()
+	for(name in names(draw))
+	{
+		# Extract "chain 1"
+		results[[name]] = as.array(draw[[name]][,,1])
+		
+		# Transpose 2D arrays
+		if(length(dim(results[[name]])) == 2)
+			results[[name]] = t(results[[name]])
+	}
+	return(results)
+}
+results = make_list(draw)
