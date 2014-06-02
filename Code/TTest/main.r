bdeb8adfeca18a998833df46954b45baca971e8e
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-06-02 12:32:15 +1200

    Seed with 42 (didn't port to use_jags script)

--- Code/TTest/main.r
+++ Code/TTest/main.r
@@ -1,16 +1,27 @@
 # Load the rjags library
 library('rjags')
 
-set.seed(123)
-
-# Load my make_list function
-source('../make_list.r')
+# Convert to a list
+make_list <- function(draw)
+{
+	results = list()
+	for(name in names(draw))
+	{
+		# Extract "chain 1"
+		results[[name]] = as.array(draw[[name]][,,1])
+		
+		# Transpose 2D arrays
+		if(length(dim(results[[name]])) == 2)
+			results[[name]] = t(results[[name]])
+	}
+	return(results)
+}
 
 # Load data from file
 source('data.txt')
 
 # Run model 1 and save results
-m = jags.model(file='model1.txt', data=list(N1=data$N1, N2=data$N2))
+m = jags.model(file='model1.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=list(N1=data$N1, N2=data$N2))
 update(m, 10000)
 draw = jags.samples(m, 50000, thin=10,
 		variable.names = c('mu1', 'mu2', 'sigma'))
@@ -22,7 +33,7 @@ chains[,3] = results$sigma
 write.table(chains, file='prior1.txt', row.names=FALSE, col.names=FALSE)
 
 # Run model 2 and save results
-m = jags.model(file='model2.txt', data=list(N1=data$N1, N2=data$N2))
+m = jags.model(file='model2.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=list(N1=data$N1, N2=data$N2))
 update(m, 10000)
 draw = jags.samples(m, 50000, thin=10,
 		variable.names = c('mu1', 'mu2', 'sigma'))
@@ -34,7 +45,7 @@ chains[,3] = results$sigma
 write.table(chains, file='prior2.txt', row.names=FALSE, col.names=FALSE)
 
 # Run model 3 and save results
-m = jags.model(file='model3.txt', data=list(N1=data$N1, N2=data$N2))
+m = jags.model(file='model3.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=list(N1=data$N1, N2=data$N2))
 update(m, 10000)
 draw = jags.samples(m, 50000, thin=10,
 		variable.names = c('mu1', 'mu2', 'sigma'))
@@ -47,7 +58,7 @@ write.table(chains, file='prior3.txt', row.names=FALSE, col.names=FALSE)
 
 
 # Run model 1 and save results
-m = jags.model(file='model1.txt', data=data)
+m = jags.model(file='model1.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=data)
 update(m, 10000)
 draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
@@ -59,7 +70,7 @@ chains[,3] = results$sigma
 write.table(chains, file='chains1.txt', row.names=FALSE, col.names=FALSE)
 
 # Run model 2 and save results
-m = jags.model(file='model2.txt', data=data)
+m = jags.model(file='model2.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=data)
 update(m, 10000)
 draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
@@ -71,7 +82,7 @@ chains[,3] = results$sigma
 write.table(chains, file='chains2.txt', row.names=FALSE, col.names=FALSE)
 
 # Run model 3 and save results
-m = jags.model(file='model3.txt', data=data)
+m = jags.model(file='model3.txt', inits=list(.RNG.seed=42, .RNG.name="base::Mersenne-Twister"), data=data)
 update(m, 10000)
 draw = jags.samples(m, 100000, thin=100,
 		variable.names = c('mu1', 'mu2', 'sigma'))
