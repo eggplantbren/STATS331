155c3a419be86bbbc2e5228d677124626809fc28
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-04-09 09:43:27 +1200

    Slimmed down use_jags.R

--- Code/use_jags.R
+++ Code/use_jags.R
@@ -1,18 +1,24 @@
 model = "model
 {
-  theta ~ dunif(0, 1)
-  x ~ dbin(theta, N)
+    mu ~ dnorm(0, 1/1000^2)
+    sigma ~ dgamma(0.001, 0.001)
+    for(i in 1:N)
+    {
+        x[i] ~ dnorm(mu, 1/sigma^2)
+    }
+
+    a[1] ~ dunif(1, 5)
+    a[2] ~ dunif(3, 4)
 }
 "
 
-# The data (use NA for no data)
-data = list(x=2, N=5)
+# A dataset
+data = list(x = c(0.044103595329532, -0.922186227194728, 0.0960010565013454, 
+-1.12828522333698, 1.36291783960003, -2.23065969145022, -0.93027082294735, 
+-2.07175801570109, 1.20069146950599, 3.3937627148158), N = 10)
 
 # Variables to monitor
-variable_names = c("theta", "x")
-
-# How many burn-in steps?
-burn_in = 1000
+variable_names = c("mu", "sigma", "a")
 
 # How many proper steps?
 steps = 100000
@@ -20,9 +26,7 @@ steps = 100000
 # Thinning?
 thin = 10
 
-# Random number seed
-seed = 42
-
+# Import the rjags library
 library("rjags")
 
 # Write model out to file
@@ -33,11 +37,8 @@ close(fileConn)
 # Create a JAGS model object
 jm = jags.model("model_temp.txt", data)
 
-# Do some burn-in
-update(jm, burn_in)
-
 # Do some MCMC
-results = jags.samples(jm, variable_names, steps, thin)
+results = coda.samples(jm, variable_names, steps, thin=thin)
 
-# Plot a trace plot
-plot(as.vector(results$theta), type="l")
+# Extract chains as data frame
+results = as.data.frame(results[[1]])
