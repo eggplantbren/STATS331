8f4c32fa9a8d2a3daae4024b3e55754a7608960a
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-24 12:14:48 +1200

    New slide and plot

--- NewSlides/plots.py
+++ NewSlides/plots.py
@@ -17,3 +17,13 @@ plt.xlabel("$x$")
 plt.ylabel("Probability")
 plt.title("A discrete probability distribution")
 plt.savefig("images/geometric.pdf", bbox_inches="tight")
+
+plt.clf()
+x = np.linspace(-5.0, 5.0, 1001)
+plt.plot(x, np.exp(-0.5*x**2)/np.sqrt(2.0*np.pi))
+plt.xlabel("$x$")
+plt.ylabel("Probability Density")
+plt.title("A continuous probability distribution")
+plt.savefig("images/normal.pdf", bbox_inches="tight")
+
+
