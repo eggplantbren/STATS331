c4149647c515f871a15bbbfb93e105c1f22555c3
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-24 12:33:00 +1200

    Another plot and slide

--- NewSlides/plots.py
+++ NewSlides/plots.py
@@ -20,10 +20,21 @@ plt.savefig("images/geometric.pdf", bbox_inches="tight")
 
 plt.clf()
 x = np.linspace(-5.0, 5.0, 1001)
-plt.plot(x, np.exp(-0.5*x**2)/np.sqrt(2.0*np.pi))
+p = np.exp(-0.5*x**2)/np.sqrt(2.0*np.pi)
+plt.plot(x, p)
+plt.ylim(0.0)
 plt.xlabel("$x$")
 plt.ylabel("Probability Density")
 plt.title("A continuous probability distribution")
 plt.savefig("images/normal.pdf", bbox_inches="tight")
 
+plt.plot([1.0, 1.0], [0.0, np.exp(-0.5*1.0**2)/np.sqrt(2.0*np.pi)],
+         color="r")
+plt.plot([2.0, 2.0], [0.0, np.exp(-0.5*2.0**2)/np.sqrt(2.0*np.pi)],
+         color="r")
+
+subset = (x > 1.0) & (x < 2.0)
+plt.fill_between(x[subset],
+                 y1=np.zeros(len(x[subset])), y2=p[subset], color="r", alpha=0.3)
+plt.savefig("images/integral.pdf", bbox_inches="tight")
 
